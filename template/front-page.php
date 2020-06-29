78fa28cf85bcabcf52579fcee6a8d9ce330d01f7
Tim <github@timgreen.ws>
2020-06-30 08:24:32 +1000

    created front page template with maxslider shortcode implementation

---
+++ template/front-page.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template Name: Front page
+ */
+get_header(); ?>
+
+<?php
+	$slider_id = get_post_meta( get_queried_object_id(), 'booth_woo_front_slider_id', true );
+	if ( $slider_id && function_exists( 'MaxSlider' ) ) {
+		echo apply_filters( 'booth_woo_front_page_maxslider_html', do_shortcode( sprintf( '[maxslider id="%s" template="home"]', intval( $slider_id ) ) ) ); // WPCS: XSS ok.
+	}
+?>
+
+<main id="site-content" class="main widget-sections">
+	<?php dynamic_sidebar( 'frontpage' ); ?>
+</main>
+
+<?php get_footer();
