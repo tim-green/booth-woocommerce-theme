aba538dd1e5c3d732b1d6fae4c46c7004880493e
Tim <github@timgreen.ws>
2020-06-29 17:50:55 +1000

    Booth functions and definitions

--- functions.php
+++ functions.php
@@ -1,15 +1,14 @@
 <?php
 
-$theme_version = '2.2.0';
+// Booth functions and definitions
+if (!defined ('Booth_Woo')){
+	define('Booth_Woo', 'booth-woo');
+}
 
-	/**
-	 * Include Theme Customizer
-	 *
-	 * @since v1.0
-	 */
-	$theme_customizer = get_template_directory() . '/inc/customizer.php';
-	if ( is_readable( $theme_customizer ) ) {
-		require_once $theme_customizer;
+if (! defined('Booth_Whitelabel')){
+	define('Booth_Whitelabel', false);
+}
+
 if (! function_exists('booth_setup')):
 
 	/*
@@ -157,14 +156,6 @@ function booth_woo_has_sidebar() {
 			$has_sidebar = true;
 		}
 
-
-	/**
-	 * Set the content width based on the theme's design and stylesheet
-	 *
-	 * @since v1.0
-	 */
-	if ( ! isset( $content_width ) ) {
-		$content_width = 800;
 		if ( is_product() ) {
 			$has_sidebar = false;
 		}
@@ -329,122 +320,12 @@ function booth_woo_hide_single_featured_save_post( $post_id ) {
 	if ( ! in_array( get_post_type( $post_id ), $hide_featured_support, true ) ) {
 		return;
 	}
-	add_filter( 'the_password_form', 'themes_starter_password_form' );
 
-
-	if ( ! function_exists( 'themes_starter_comment' ) ) :
-		/**
-		 * Style Reply link
-		 *
-		 * @since v1.0
-		 */
-		function themes_starter_replace_reply_link_class( $class ) {
-			$output = str_replace( "class='comment-reply-link", "class='comment-reply-link btn btn-outline-secondary", $class );
-			return $output;
-		}
-		add_filter( 'comment_reply_link', 'themes_starter_replace_reply_link_class' );
-
-		/**
-		 * Template for comments and pingbacks:
-		 * add function to comments.php ... wp_list_comments( array( 'callback' => 'themes_starter_comment' ) );
-		 *
-		 * @since v1.0
-		 */
-		function themes_starter_comment( $comment, $args, $depth ) {
-			global $theme_dateformat, $theme_timeformat;
-
-			$GLOBALS['comment'] = $comment;
-			switch ( $comment->comment_type ) :
-				case 'pingback' :
-				case 'trackback' :
-			?>
-			<li class="post pingback">
-				<p><?php _e( 'Pingback:', 'my-theme' ); ?> <?php comment_author_link(); ?><?php edit_comment_link( __( 'Edit', 'my-theme' ), '<span class="edit-link">', '</span>' ); ?></p>
-			<?php
-					break;
-				default :
-			?>
-			<li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
-				<article id="comment-<?php comment_ID(); ?>" class="comment">
-					<footer class="comment-meta">
-						<div class="comment-author vcard">
-							<?php
-								$avatar_size = 136;
-								if ( '0' !== $comment->comment_parent ) {
-									$avatar_size = 68;
-								}
-								echo get_avatar( $comment, $avatar_size );
-
-								/* translators: 1: comment author, 2: date and time */
-								printf( __( '%1$s, %2$s', 'my-theme' ),
-									sprintf( '<span class="fn">%s</span>', get_comment_author_link() ),
-									sprintf( '<a href="%1$s"><time datetime="%2$s">%3$s</time></a>',
-										esc_url( get_comment_link( $comment->comment_ID ) ),
-										get_comment_time( 'c' ),
-										/* translators: 1: date, 2: time */
-										//sprintf( __( '%1$s - %2$s', 'my-theme' ), get_comment_time( $theme_dateformat ), get_comment_time( $theme_timeformat ) )
-										sprintf( __( '%1$s ago', 'my-theme' ), human_time_diff( get_comment_time( 'U' ), current_time( 'timestamp' ) ) )
-									)
-								);
-							?>
-
-							<?php edit_comment_link( __( 'Edit', 'my-theme' ), '<span class="edit-link">', '</span>' ); ?>
-						</div><!-- .comment-author .vcard -->
-
-						<?php if ( '0' === $comment->comment_approved ) : ?>
-							<em class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.', 'my-theme' ); ?></em>
-							<br />
-						<?php endif; ?>
-
-					</footer>
-
-					<div class="comment-content"><?php comment_text(); ?></div>
-
-					<div class="reply">
-						<?php comment_reply_link( array_merge( $args, array( 'reply_text' => __( 'Reply', 'my-theme' ) . ' <span>&darr;</span>', 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
-					</div><!-- .reply -->
-				</article><!-- #comment-## -->
-
-			<?php
-					break;
-			endswitch;
-
-		}
 	if ( isset( $_POST['_booth_woo_hide_single_featured_meta_box_nonce'] ) && wp_verify_nonce( sanitize_key( $_POST['_booth_woo_hide_single_featured_meta_box_nonce'] ), 'booth_woo_hide_single_featured_nonce' ) ) {
 		update_post_meta( $post_id, 'booth_woo_hide_single_featured', isset( $_POST['booth_woo_hide_single_featured'] ) ); // Input var okay.
 	}
 }
 
-		/**
-		 * Custom Comment form
-		 *
-		 * @since v1.0
-		 * @since v1.1: Added 'submit_button' and 'submit_field'
-		 * @since v2.0.2: Added '$consent' and 'cookies'
-		 */
-		function themes_starter_custom_commentform( $args = array(), $post_id = null ) {
-			if ( null === $post_id ) {
-				$post_id = get_the_ID();
-			}
-
-			$commenter = wp_get_current_commenter();
-			$user = wp_get_current_user();
-			$user_identity = $user->exists() ? $user->display_name : '';
-
-			$args = wp_parse_args( $args );
-
-			$req = get_option( 'require_name_email' );
-			$aria_req = ( $req ? " aria-required='true' required" : '' );
-			$consent  = ( empty( $commenter['comment_author_email'] ) ? '' : ' checked="checked"' );
-			$fields = array(
-				'author'  => '<div class="form-group"><label for="author">' . __( 'Name', 'my-theme' ) . ( $req ? '<span class="required">*</span>' : '' ) . '</label>' . 
-							'<input type="text" id="author" name="author" class="form-control" value="' . esc_attr( $commenter['comment_author'] ) . '"' . $aria_req . ' /></div>',
-				'email'   => '<div class="form-group"><label for="email">' . __( 'Email', 'my-theme' ) . ( $req ? '<span class="required">*</span>' : '' ) . '</label>' . 
-							'<input type="email" id="email" name="email" class="form-control" value="' . esc_attr( $commenter['comment_author_email'] ) . '"' . $aria_req . ' /></div>',
-				'url'     => '',
-				'cookies' => '<p class="comment-form-cookies-consent"><input id="wp-comment-cookies-consent" name="wp-comment-cookies-consent" type="checkbox" value="yes"' . $consent . ' /> ' .
-							 '<label for="wp-comment-cookies-consent">' . __( 'Save my name, email, and website in this browser for the next time I comment.', 'my-theme' ) . '</label></p>',
-			);
 if ( ! function_exists( 'booth_woo_get_template_part' ) ) :
 /*
  * Load a template part into a template, optionally passing an associative array that will be available as variables.
