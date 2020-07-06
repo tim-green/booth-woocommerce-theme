92a71d64804092c5cb4ff3925f8019055989bc02
Tim <github@timgreen.ws>
2020-07-07 08:33:01 +1000

    added magnific plugin to the theme

---
+++ assets/js/plugins/magnific/magnific-init.js
@@ -0,0 +1,30 @@
+jQuery(function( $ ) {
+	'use strict';
+
+	/* Image Lightbox */
+	$( ".booth-woo-lightbox, a[data-lightbox^='gal']" ).magnificPopup({
+		type: 'image',
+		mainClass: 'mfp-with-zoom',
+		gallery: {
+			enabled: true
+		},
+		zoom: {
+			enabled: true
+		},
+		image: {
+			titleSrc: function (item) {
+				var $item = item.el;
+				var $parentCaption = $item.parents('.wp-caption').first();
+
+				if ($item.attr('title')) {
+					return $item.attr('title');
+				}
+
+				if ($parentCaption) {
+					return $parentCaption.find('.wp-caption-text').text();
+				}
+			},
+		},
+	} );
+
+});
